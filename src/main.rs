a471d79934440b81e9ab4bc2151ce1b65c0afafd
Matthew Kousoulas <shaman.kous@gmail.com>
2018-11-04 21:14:19 -0500

    added runtime system, most functionality is now in drawer or runtime

--- src/main.rs
+++ src/main.rs
@@ -24,64 +24,53 @@ extern crate termion;
 use termion::screen::*;
 use termion::terminal_size;
 
-extern crate lib;
-use lib::entry::*;
-use lib::stack::*;
-use lib::operator::*;
+extern crate stablib;
+use stablib::entry::*;
+// use stablib::stack::*;
+use stablib::operator::*;
+use stablib::runtime::*;
 use ::drawer::*;
 
 pub mod drawer;
 
 fn main() {
-	let mut stack: Stack = Stack::new("main".to_string());
-	let operators = OpMap::default();
-	{
-		let mut screen = AlternateScreen::from(io::stdout());//.into_raw_mode().unwrap());
-		let (scr_x, scr_y) = terminal_size().unwrap();
-		let mut status: Option<String> = Some("Welcome to RPN calc".to_string());
+	let mut r = Runtime::new("Welcome to Stabacus!");
+	let mut screen = AlternateScreen::from(io::stdout());//.into_raw_mode().unwrap());
+	let (scr_x, scr_y) = terminal_size().unwrap();
 
-		'main: loop {
-			reset_screen();
-			draw_box(1, 1, scr_x, scr_y-5, DUB);
-			draw_stack(3, 3, 20, 20, &stack);
-			if let Some(s) = status {
-				draw_status(&s, 2, scr_y-1);
-				status = None;
-			}
-			draw_prompt(scr_y);
+	'main: loop {
+		draw_runtime(scr_x, scr_y, &mut r);
+		// draw_screen(scr_x, scr_y);
+		// draw_stack(3, 2, 21, scr_y-4, &r.active());
+		// if let Some(s) = r.status {
+		// 	draw_status(&s, 2, scr_y-1);
+		// 	r.status = None;
+		// }
+		// draw_prompt(scr_y);
+		io::stdout().flush().unwrap();
 
-			io::stdout().flush().unwrap();
+		let mut s = String::new();
+		io::stdin().read_line(&mut s)
+			.expect("Failed to read line.");
 
-			let mut s = String::new();
-			io::stdin().read_line(&mut s)
-				.expect("Failed to read line.");
+		let e = match s.trim().parse::<ParseType>()
+			.map(|i| OpMap::get_entry(&r.operators, i)) {
+				Ok(val) => val,
+				Err(_) => panic!("parse error: {}", s),
+			};
+		r.active().push(e);
 
-			// if i < 10 {
-			// 	thread::sleep(time::Duration::from_secs(1));
-			// 	stack.push(Entry::Int(i));
-			// 	i += 1;
-			// } else {
-			// 	thread::sleep(time::Duration::from_secs(1));
-			// 	stack.push(Entry::Die);
-			// }
-
-			stack.push(match s.trim().parse::<ParseType>()
-					   .map(|i| OpMap::get_entry(&operators, i)) {
-						   Ok(val) => val,
-						   Err(_) => panic!("parse error: {}", s),
-					   });
-
-			'proc: loop {
-				match stack.peek() {
-					Some(Entry::Panic(_)) => status = Some(stack.panic()),
-					Some(Entry::Op(_)) => stack.operate(),
-					Some(Entry::Die) => break 'main,
-					Some(Entry::Pop) => { stack.pop(); },
-					_ => break 'proc,
-				}
+		'proc: loop {
+			match r.active().peek() {
+				Some(Entry::Panic(_)) => r.status = Some(r.active().panic()),
+				Some(Entry::Op(_)) => r.active().operate(),
+				Some(Entry::Die) => break 'main,
+				Some(Entry::Pop) => { r.active().pop(); },
+				// Some(Entry::Id) => (),
+				_ => break 'proc,
 			}
 		}
-		reset_screen();
-		screen.flush().unwrap();
 	}
+	reset_screen();
+	screen.flush().unwrap();
 }
